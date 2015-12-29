2857a6bd5a595780f2ddca81c02203228c1ddeb4
Paul Vidal <paul.vidal@jmtech.pe>
2015-12-29 15:28:55 -0500

    route helpers

--- files/helpers.php
+++ files/helpers.php
@@ -5,3 +5,34 @@ if (! function_exists('multiAuth')) {
 		return Illuminate\Support\Facades\Auth::guard($guard);
 	}
 }
+
+if (! function_exists('multiAuthLoginRoutes')) {
+	function multiAuthLoginRoutes($controller = "Auth\AuthController"){
+		app('router')->get('login', "$controller@showLoginForm");
+	    app('router')->get('logout', "$controller@logout");
+	    app('router')->post('login', "$controller@login");
+	}
+}
+
+if (! function_exists('multiAuthRegisterRoutes')) {
+	function multiAuthRegisterRoutes($controller = "Auth\AuthController"){
+	    app('router')->get('register', "$controller@showRegistrationForm");
+	    app('router')->post('register', "$controller@register");
+	}
+}
+
+if (! function_exists('multiAuthPasswordRoutes')) {
+	function multiAuthPasswordRoutes($controller = "Auth\AuthController"){
+	    app('router')->get('password/reset/{token?}', "$controller@showResetForm");
+	    app('router')->post('password/email', "$controller@sendResetLinkEmail");
+	    app('router')->post('password/reset', "$controller@reset");
+	}
+}
+
+if (! function_exists('multiAuthRoutes')) {
+	function multiAuthRoutes($controller = "Auth\AuthController"){
+		multiAuthLoginRoutes($controller);
+		multiAuthRegisterRoutes($controller);
+		multiAuthPasswordRoutes($controller);
+	}
+}
