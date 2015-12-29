dc61b58a09f86b1cf784db5645a3602b55f6af8d
Paul Vidal <paul.vidal@jmtech.pe>
2015-12-29 15:57:21 -0500

    changed route actions to be more readable

--- files/helpers.php
+++ files/helpers.php
@@ -8,24 +8,24 @@ if (! function_exists('multiAuth')) {
 
 if (! function_exists('multiAuthLoginRoutes')) {
 	function multiAuthLoginRoutes($controller = "Auth\AuthController"){
-		app('router')->get('login', "$controller@showLoginForm");
-	    app('router')->get('logout', "$controller@logout");
-	    app('router')->post('login', "$controller@login");
+		app('router')->get('login', "$controller@getLogin");
+	    app('router')->get('logout', "$controller@getLogout");
+	    app('router')->post('login', "$controller@postLogin");
 	}
 }
 
 if (! function_exists('multiAuthRegisterRoutes')) {
 	function multiAuthRegisterRoutes($controller = "Auth\AuthController"){
-	    app('router')->get('register', "$controller@showRegistrationForm");
-	    app('router')->post('register', "$controller@register");
+	    app('router')->get('register', "$controller@getRegister");
+	    app('router')->post('register', "$controller@postRegister");
 	}
 }
 
 if (! function_exists('multiAuthPasswordRoutes')) {
 	function multiAuthPasswordRoutes($controller = "Auth\AuthController"){
-	    app('router')->get('password/reset/{token?}', "$controller@showResetForm");
-	    app('router')->post('password/email', "$controller@sendResetLinkEmail");
-	    app('router')->post('password/reset', "$controller@reset");
+	    app('router')->get('password/reset/{token?}', "$controller@getEmail");
+	    app('router')->post('password/email', "$controller@postEmail");
+	    app('router')->post('password/reset', "$controller@getReset");
 	}
 }
 
